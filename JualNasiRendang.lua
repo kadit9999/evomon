4ef84a4e6ea3a7e287b6b88416785e103ecfe8e3
kadit9999 <moiacc833@gmail.com>
2026-06-29 12:55:24 +0700

    Create JualNasiRendang.lua

---
+++ JualNasiRendang.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JualNasiRendang/nasirendang-evomon/refs/heads/main/nasirendang-evomon.lua"))()
+
+
+-- join discord for password : https://discord.gg/vrzg9YaNPj
